4da3268da93ff9854edb15c20f35616ecb4fd80c
lnc <lnc@DIN76000526.corp.capgemini.com>
2016-10-19 16:03:57 +0530

    First commit

---
+++ DemoApp/src/main/java/com/capg/com/GitDemo.java
@@ -0,0 +1,9 @@
+package com.capg.com;
+
+public class GitDemo 
+{
+    public void display()
+    {
+    	System.out.println("Welcome to git world");
+    }
+}
---
+++ DemoApp/src/main/java/com/capg/com/MainClass.java
@@ -0,0 +1,11 @@
+package com.capg.com;
+
+public class MainClass {
+
+	public static void main(String[] args)
+	{
+		GitDemo demo=new GitDemo();
+		demo.display();
+	}
+
+}
